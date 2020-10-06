8788d92adf2ddb84fb89adca1e172df8523f78d1
oscar <oll301189@gmail.com>
2020-10-06 20:31:27 -0300

    add apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello word 2");
+import "reflect-metadata";
+import {startServer} from "./app";
+
+async function main(){
+    const app = await startServer();
+    app.listen(3000);
+    console.log("Server on port", 3000);
+}
+
+main();
