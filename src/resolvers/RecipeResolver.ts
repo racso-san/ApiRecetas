5a6fac2dcddf3df5a4d5a419aadd4c3ff6b1896a
oscar <oll301189@gmail.com>
2020-10-08 20:31:27 -0300

    fix add recipe resolver

--- src/resolvers/RecipeResolver.ts
+++ src/resolvers/RecipeResolver.ts
@@ -1,6 +1,20 @@
-import { Resolver, Query, Mutation, Arg, ObjectType } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg, ObjectType, Field, InputType } from 'type-graphql';
+//import { IsEmail, IsNotEmpty } from 'class-validator';
 
-import {Recipe} from "../entity/Receta";
+import { Recipe } from '../entity/Receta';
+
+@InputType() // Se pasa como argumento desde graphql
+class RecipeInput {
+
+    @Field()
+    name!: string
+
+    @Field()
+    description!:string
+
+    @Field()
+    ingredients!: string
+}
 
 @Resolver()
 export class RecipeResolver {
@@ -10,14 +24,12 @@ export class RecipeResolver {
         return Recipe.find()
     }
 
-    @Mutation(() => Boolean)
+    @Mutation(() => Recipe)
     async createRecipe(
-        @Arg("name") name: string,
-        @Arg("description") description: string,
-        @Arg("ingredients") ingredients: string
+        @Arg("variables", () => RecipeInput ) variables: RecipeInput
     ) {
-        await Recipe.insert({name,description,ingredients})
-        return true
+        const newRecipe =  Recipe.create(variables);
+        return await newRecipe.save();
     }
 
 }
