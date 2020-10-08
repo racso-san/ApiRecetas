20797eccaf198554b9cb8fe34abcc3cbb85f637a
oscar <oll301189@gmail.com>
2020-10-08 19:46:12 -0300

    get info recipes

--- src/entity/Receta.ts
+++ src/entity/Receta.ts
@@ -1,20 +1,27 @@
 import {Entity, Column, PrimaryGeneratedColumn, BaseEntity } from "typeorm";
+import { Field, ObjectType } from 'type-graphql';
 
+@ObjectType()
 @Entity()
 export class Recipe extends BaseEntity {
 
+    @Field() // Indicar que tambien es un campo de graphql
     @PrimaryGeneratedColumn()
     id!: number; // Generar autom el id
 
+    @Field()
     @Column()
     name!: String;
 
+    @Field()
     @Column()
     description!: String;
 
+    @Field()
     @Column()
     ingredients!: String;
-    /* 
+    /*
+    @Field() 
     @Column()
     category!: number; */
 }
