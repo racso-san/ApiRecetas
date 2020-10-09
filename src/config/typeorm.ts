d11b071198f97ebe824b1d7ed01e2135c48c44e0
oscar <oll301189@gmail.com>
2020-10-09 19:24:54 -0300

    fix connected database

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -2,17 +2,23 @@ import {createConnection} from 'typeorm'
 import path from 'path'
 
 export async function connect() {
-  await createConnection({
-    type:"mysql",
-    host: "localhost",
-    port: 3306,
-    username: "oscar", //root
-    password: "1234",
-    database: "recetas-bd",
-    entities: [
-      path.join(__dirname, '../entity/**/**.ts')
-    ],
-    synchronize: true
-  });
-  console.log('Database is Connected')
+  try {
+    await createConnection({
+      type:"mysql",
+      host: "localhost",
+      port: 3306,
+      username: "oscar", //root
+      password: "1234",
+      database: "recetas-bd",
+      entities: [
+        path.join(__dirname, '../entity/**/**.ts')
+      ],
+      synchronize: true
+    });
+    console.log('Database is Connected')
+  } catch(err) {
+      console.log(err);
+      process.exit(1); // Detener la app
+  }
+ 
 }
